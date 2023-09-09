1ea25f6ca8595b1fc505af2dbd9da16a38dd0f27
Vignesh Sivarajah <vigneshsivarajah@Vigneshs-MacBook-Pro.local>
2023-09-09 18:21:09 +0200

    test

--- dag_new.py
+++ dag_new.py
@@ -48,7 +48,7 @@ dag = DAG(
 
 submit = SparkKubernetesOperator(
     task_id='spark_transform_data',
-    namespace='spark-jobs',
+    namespace='spark-operator',
     application_file='spark-pi.yaml',
     kubernetes_conn_id='kubernetes_default',
     do_xcom_push=True,
